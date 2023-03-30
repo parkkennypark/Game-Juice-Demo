38702b8d741a00eb3fe48cd30971d06b874813b7
Kenny Park <parkkennypark@gmail.com>
2023-03-30 12:47:21 -0400

    Update Player.gd

--- Player.gd
+++ Player.gd
@@ -6,6 +6,10 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@export var projectile_scene : PackedScene
+@export var fire_particles_scene : PackedScene
+@export var muzzle_flash_animation_player : AnimationPlayer
+
 @onready var camera = get_viewport().get_camera_3d()
 
 func _process(delta: float) -> void:
@@ -21,8 +25,13 @@ func _process(delta: float) -> void:
 	$Pointer/Rotation.scale.z = dist
 	$Pointer/Rotation/Geometry.material_override.uv1_scale = Vector3.ONE * dist * 2
 	
-	var target_rot = Quaternion.from_euler(Vector3(0, angle, 0))
-	$Model.global_transform.basis = $Model.global_transform.basis.slerp(target_rot, delta * 5)
+	var player_angle = Vector3.BACK.signed_angle_to(vec_to_pointer.normalized(), Vector3.UP)
+	var target_rot = Quaternion.from_euler(Vector3(0, player_angle, 0))
+	global_transform.basis = global_transform.basis.slerp(target_rot, delta * 5)
+#	$Model.global_transform.basis = $Model.global_transform.basis.slerp(target_rot, delta * 5)
+	
+	if Input.is_action_just_pressed("fire"):
+		fire_projectile()
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -58,3 +67,26 @@ func get_mouse_pos() -> Vector3:
 	var distance = -origin.y/direction.y
 	var position = origin + direction * distance
 	return position
+
+
+func fire_projectile():
+	$Model/AnimationPlayer.play("Fire")
+	$Model/AnimationPlayer.seek(0, false)
+	
+	var projectile = projectile_scene.instantiate()
+	get_tree().root.add_child(projectile)
+	projectile.global_position = global_position + Vector3.UP * 0.5
+	projectile.global_rotation = $Model.global_rotation
+	
+	var particles = fire_particles_scene.instantiate()
+	get_tree().root.add_child(particles)
+	particles.emitting = true
+	particles.global_position = $Model/MuzzleFlash.global_position
+	particles.global_rotation = $Model.global_rotation
+	
+	flash_muzzle()
+
+func flash_muzzle():
+	muzzle_flash_animation_player.play("Flash")
+	muzzle_flash_animation_player.seek(0, true)
+	
